5cac05512f5ccac5d1c7c690d2b17202b2c6f0c6
Dylan Brain <dylanbrain@googlemail.com>
2023-07-18 21:05:00 +0100

    added favicon and updated logo crop

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <div className="bg-gray-200 w-full flex justify-center h-32">
       <div className="w-11/12 max-w-screen-xl flex justify-between items-center">
-        <img class="w-64" src={logo} alt="logo" />
+        <img class="w-52" src={logo} alt="logo" />
         <a
           href="mailto:jonathan@crestenvironmental.co.uk"
           class="text-base px-4 py-2 rounded-lg text-crest-green bg-crest-blue hover:text-white"
